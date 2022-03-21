3e3d03e7d8bab2a33432ece9e40f52aa5f003146
Kate Johnson <johnson.kat.g@gmail.com>
2022-03-21 10:53:39 -0400

    functional api response

--- crypto-calculator/src/components/PortfolioContainer.jsx
+++ crypto-calculator/src/components/PortfolioContainer.jsx
@@ -11,63 +11,59 @@ export default function PortfolioContainer() {
     active_currency: null,
     amount: ''
   })
-  const [active_currency, setActiveCurrency] = useState(null)
-  const [search_results, setSearch] = useState([])
-  const [amount, setAmount] = useState('')
-  const [portfolio, setPortfolio] = useState([])
+  // const [active_currency, setActiveCurrency] = useState(null)
+  // const [search_results, setSearch] = useState([])
+  // const [amount, setAmount] = useState('')
+  // const [portfolio, setPortfolio] = useState([])
   
-  const [calculate, showCalculate] = useState(false)
+  // const [calculate, showCalculate] = useState(false)
   
 
-  const handleChange = (e) => {
-    axios.post('http://localhost:3000/search', {
-      search: e.target.value
-    }).then(data => {
-      setSearch(data.data.currencies)
-    }).catch(err => {
+  const handleChange = async (e) => {
+    try {
+      const data = await axios.post('http://localhost:3000/search', { search: e.target.value })
+      setInfo(state => ({ ...state, search_results: [...data.data.currencies] }))
+    } catch(err) {
       console.log(err)
-    })
+    }
   }
 
 const handleSelect = (curr, e) => {
   e.preventDefault()
-  const activeCurrency = search_results.find( item => item.id == curr.id)
-  setActiveCurrency(activeCurrency)
-  showCalculate(true)
+  const activeCurrency = info.search_results.find(item => item.id == curr.id)
+  setInfo(state => ({ ...state, search_results: [], active_currency: activeCurrency }))
 }
 
-const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    
-    axios.post('http://localhost:3000/calculate', {
-      id: active_currency.id,
-      amount: amount
-    }).then(data => {
-      console.log(data)
-      setInfo({ 
-        portfolio: [...info.portfolio, data.data],
-        search_results: search_results,
-        active_currency: active_currency,
-        amount: amount
-      })
-    }).catch(err => {
+    try {
+      let currency = info.active_currency
+      let amount = info.amount
+      const data = await axios.post('http://localhost:3000/calculate', { id: currency.id, amount: amount })
+      setInfo(state => ({
+        ...state,
+        portfolio: [...state.portfolio, data.data],
+        search_results: [],
+        active_currency: null
+      }))
+    } catch (err) {
       console.log(err)
-    })
+    }
   }
-
+console.log(info)
   const handleAmount = (e) => {
-    setAmount(e.target.value)
+    setInfo(state => ({ ...state, amount: e.target.value }))
   }
 
   return (
     <div>
-      {!calculate && <Search
-        search_results={search_results}
+      {!info.active_currency && <Search
+        search_results={info.search_results}
         handleChange={handleChange}
         handleSelect={handleSelect} />}
-      {calculate && <Calculate
+      {info.active_currency && <Calculate
         amount={info.amount}
-        active_currency={active_currency}
+        active_currency={info.active_currency}
         handleChange={handleAmount}
         handleSubmit={handleSubmit} />}
     </div>
